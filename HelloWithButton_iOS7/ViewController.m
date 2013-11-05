ffe878d43053db05a8da073ea1d633f8bfa74a1d
Carlo Fantozzi <carlo.fantozzi@gmail.com>
2013-11-05 12:53:41 +0100

    UI initialization: creation of the UI objects

--- HelloWithButton_iOS7/ViewController.m
+++ HelloWithButton_iOS7/ViewController.m
@@ -18,6 +18,26 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    // Create the TextView (called "label" in iOS)
+    CGRect labelFrame = CGRectMake(119.0, 27.0, 186.0, 21.0);
+    tv = [[UILabel alloc] initWithFrame:labelFrame];
+    [tv setText:@"Press the button, please"];
+    
+    // Create the Button
+    CGRect buttonFrame = CGRectMake(20.0, 20.0, 91.0, 37.0);
+    bu = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    bu.frame = buttonFrame;
+    [bu setTitle:@"Press me" forState:UIControlStateNormal];
+    
+    // Set the action to be performed when the button is pressed
+    [bu addTarget:self
+           action:@selector(buttonClicked:)
+ forControlEvents:UIControlEventTouchUpInside];
+	
+    // Add the button and the label to the view
+    [self.view addSubview:bu];
+    [self.view addSubview:tv];
 }
 
 - (void)didReceiveMemoryWarning
